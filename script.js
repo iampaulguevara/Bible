13e560e682e32e1e3570922234c37f2406fae635
Pablo G <pabloguevara1107@gmail.com>
2022-10-10 19:45:25 -0700

    added Gospel of the Day

--- script.js
+++ script.js
@@ -10,6 +10,34 @@
 // Inserts the text into the HTML DOM
 // window.document.getElementById("scripture").innerHTML = text;
 
+var rand = Math.random();
+var rand2 = Math.random();
+var gospelOfTheDay;
+var chapterOfTheDay;
+
+if(rand < 0.25){
+    // 28 books
+    gospelOfTheDay = "Matthew"
+    chapterOfTheDay = Math.round(rand2*27+1)
+}
+else if(rand < 0.5){
+    // 16 books
+    gospelOfTheDay = "Mark"
+    chapterOfTheDay = Math.round(rand2*15+1)
+}
+else if(rand < 0.75){
+    // 24 books
+    gospelOfTheDay = "Luke"
+    chapterOfTheDay = Math.round(rand2*23+1)
+}
+else{
+    // 21 books
+    gospelOfTheDay = "John"
+    chapterOfTheDay = Math.round(rand2*20+1)
+};
+
+window.document.getElementById("book").innerHTML = gospelOfTheDay + " " + chapterOfTheDay;
+
 var text;
 
 function readTextFile(file)
@@ -28,7 +56,12 @@ function readTextFile(file)
         }
     }
     rawFile.send(null);
-}
+};
+
+
+// randomly generated Gospel of the Day (actually changes after each refresh) - before any searches/updates are requested
+readTextFile("Books/"+gospelOfTheDay+"/"+chapterOfTheDay+".txt");
+window.document.getElementById("scripture").innerHTML = text;
 
 
 // updates the page when the user clicks on the search button
@@ -54,4 +87,3 @@ search.addEventListener("keypress",function(event){
         document.getElementById("search-button").click()
     }
 });
-
